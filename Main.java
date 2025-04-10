72cfcf5ce2af9b10448137e264ce04f3fa3717e7
Edwin-l-H <edwinlh2789@nycstudents.net>
2025-04-10 18:12:29 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ public class Main
         System.out.println(APCalendar.numberOfLeapYears(2000, 2025));
         System.out.println(APCalendar.dayOfWeek(1, 5, 2019));
         System.out.println(APCalendar.dayOfWeek(1, 10, 2019));
+        System.out.println(APCalendar.dayOfWeek(3, 1, 2017));
+        System.out.println(APCalendar.dayOfWeek(3, 1, 2016));
     }
 }
